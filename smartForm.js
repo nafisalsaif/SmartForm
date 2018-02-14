ef9ee304790ebc5549a43a1e44261ceeab4648e8
nafisalsaif <35512997+nafisalsaif@users.noreply.github.com>
2018-02-14 09:29:37 +0600

    Add files via upload

--- smartForm.js
+++ smartForm.js
@@ -7,14 +7,26 @@ var userData = {
 	css:{likes:[],dislikes:[]},
 	js:{likes:[],dislikes:[]},
 	strength:{css:'',js:'',html:''},
-	currntQuestion:'welcome'
+	currntQuestion:'#welcome'
 
 };
+if(localStorage.getItem('userData')){
+userData=JSON.parse(localStorage.getItem('userData'));
+$(userData.currntQuestion).show();
+$('#welcome').hide();
+$('#name').val(userData.name);
+$('#email').val(userData.email);
+
+}else{
+	localStorage.setItem('userData',JSON.stringify(userData));
+}
 $('#start').click(function(){
 	console.log("start");
 	$('#welcome').hide();
     $('#q1').show();
-    $('#q2').show();
+    userData.currntQuestion='#q1';
+    localStorage.setItem('userData',JSON.stringify(userData));
+    //$('#q2').show();
 
 });
 
@@ -26,7 +38,16 @@ $('#q1next').click(function(){
 	if($('#name').val() && $('#email').val()){
 userData.name=$('#name').val();
 userData.email=$('#email').val();
+localStorage.setItem('userData',JSON.stringify(userData));
+$('#q2').show();
+$('#q1').hide();
+console.log($('#name').val());
+console.log($('#email').val());
 }else{
 	alert('please enter name & email')
 }
+});
+
+$('#htmlopen').click(function(){
+	$('#q2a').show();
 });
